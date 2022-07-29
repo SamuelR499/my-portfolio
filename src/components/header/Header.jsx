8cc7ff6c8ff42f1fd5018bf5755d5aced65f672c
Samuelr499 <samuelribeiro2@hotmail.com>
2022-07-29 16:01:21 -0300

    refactor: adiciona thema Trybe

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,25 +10,25 @@ class Header extends Component {
       <header id="home">
         
         <div className="container header_container">
-        <div className="logo_container">
-          <div className="logo_container-img">
-            <img
-              alt="Trybe. Um novo curso para sua vida."
-              src="https://www.betrybe.com/images/logo-negative-green.svg"
-            />
+          <div className="logo_container">
+            <div className="logo_container-img">
+              <img
+                alt="Trybe. Um novo curso para sua vida."
+                src="https://www.betrybe.com/images/logo-negative-green.svg"
+              />
+            </div>
           </div>
-        </div>
-          <h5>Hello I'm</h5>
-          <h1>Samuel Ribeiro</h1>
-          <h5 className="text-light">Web development student at Trybe</h5>
-          <CTA />
-          <HeaderSocials />
-          <div className="me">
-            <img src={meTrue} alt="me" />
-          </div>
-          <a href="#contact" className="scroll_down">
-            Scroll Down
-          </a>
+            <h5>Hello I'm</h5>
+            <h1>Samuel Ribeiro</h1>
+            <h5 className="text-light">Web development student at Trybe</h5>
+            <CTA />
+            <HeaderSocials />
+            <div className="me">
+              <img src={meTrue} alt="me" />
+            </div>
+            <a href="#contact" className="scroll_down">
+              Scroll Down
+            </a>
         </div>
         
       </header>
