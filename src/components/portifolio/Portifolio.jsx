0a63820e0015003e52f43ec50fba01eda1c74c9c
Samuelr499 <samuelribeiro2@hotmail.com>
2022-07-30 11:38:03 -0300

    feat: adiciona portfolio component

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Portifolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
 
 const Portifolio = () => {
   return (
-    <section id='portifolio'>Portifolio: em construção </section>
+    <section id='portifolio'>
+      <h5>My Recente Work</h5>
+      <h2> Portfolio</h2>
+
+      <div className="container potfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="portfolio1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/SamuelR499" className='btn' target="blank">Github</a>
+          <a href="https://github.com/SamuelR499"className="btn btn-primary" target="blank">Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
