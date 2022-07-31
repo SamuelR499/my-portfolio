b8e5e41adfb8a7f81a3efd78ef9ce207869895f4
Samuelr499 <samuelribeiro2@hotmail.com>
2022-07-31 09:45:29 -0300

    fix: incomplete autentication of emails and contacts services

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,16 +8,7 @@ class Header extends Component {
   render() {
     return (
       <header id="home">
-        
         <div className="container header_container">
-          <div className="logo_container">
-            <div className="logo_container-img">
-              <img
-                alt="Trybe. Um novo curso para sua vida."
-                src="https://www.betrybe.com/images/logo-negative-green.svg"
-              />
-            </div>
-          </div>
             <h5>Hello I'm</h5>
             <h1>Samuel Ribeiro</h1>
             <h5 className="text-light">Web development student at Trybe</h5>
