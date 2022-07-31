d95ee2913e9469120cd556947cc67518d0ce34bf
Samuelr499 <samuelribeiro2@hotmail.com>
2022-07-31 01:44:51 -0300

    feat: testemunhas data e testimonials component e testimonials sttle with carrocel config

---
+++ src/assets/testemunhas.js
@@ -0,0 +1,32 @@
+import AVATAR1 from './avatar1.jpg'
+import AVATAR2 from './avatar2.jpg'
+import AVATAR3 from './avatar3.jpg'
+import AVATAR4 from './avatar4.jpg'
+
+const data = [
+    {
+			id: 1,
+			avatar: AVATAR1,
+			name: ' Mestre Atanes',
+			review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolor, quos natus nisi perferendis obcaecati, vel, dolore quam necessitatibus ipsam blanditiis aspernatur nostrum adipisci similique asperiores veritatis alias fugiat nihil.',
+    },
+		{
+			id: 2,
+			avatar: AVATAR2,
+			name: ' Mestre Wolf',
+			review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolor, quos natus nisi perferendis obcaecati, vel, dolore quam necessitatibus ipsam blanditiis aspernatur nostrum adipisci similique asperiores veritatis alias fugiat nihil.',
+    },
+		{
+			id: 3,
+			avatar: AVATAR3,
+			name: ' Jean Carlos',
+			review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolor, quos natus nisi perferendis obcaecati, vel, dolore quam necessitatibus ipsam blanditiis aspernatur nostrum adipisci similique asperiores veritatis alias fugiat nihil.',
+    },
+		{
+			id: 4,
+			avatar: AVATAR4,
+			name: 'Kaique',
+			review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolor, quos natus nisi perferendis obcaecati, vel, dolore quam necessitatibus ipsam blanditiis aspernatur nostrum adipisci similique asperiores veritatis alias fugiat nihil.',
+    },
+]
+export default data;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './Testimonials.css'
+import data from '../../assets/testemunhas'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials: em construção</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper 
+        className="container testimonials_container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        { data.map(({avatar, name, review, id})=>(
+          <SwiperSlide key={id} className="testimonial">
+            <div className="client_avatar">
+              <img src={avatar} alt={name}/>
+            </div>
+            <h5 className="client_name">{name}</h5>
+            <small className="client_review">
+              {review}
+            </small>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   )
 }
 
